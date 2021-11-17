a0e00e440180ba10bb13869899c1dd1544a0ad11
Facundo Muoio <facundo.muoio@gmail.com>
2021-11-16 22:34:34 -0300

    navbar agregada

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import NavBar  from './componentes/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Hola tuto soy Facu y todavía no se me ocurre el nombre de mi e-commerce :)</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <NavBar/>
+    </Fragment>
   );
 }
 
---
+++ src/componentes/NavBar.js
@@ -0,0 +1,17 @@
+import "./NavBar.css"
+
+const NavBar = () => {
+
+    return(
+        <nav className="container">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Contacto</a></li>
+                <li><a href="#">Perfil</a></li>
+                <li><a href="#">Tienda</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
